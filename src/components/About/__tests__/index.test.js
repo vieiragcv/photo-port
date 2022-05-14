870b604605ed113846f1224779381a8b6274f487
vieiragcv <95590262+vieiragcv@users.noreply.github.com>
2022-05-13 20:59:42 -0400

    ran 1st test

---
+++ src/components/About/__tests__/index.test.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { render, cleanup } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+
+import About from '..';
+
+afterEach(cleanup);
+
+describe('About component', () => {
+  // Test 1
+  it('renders', () => {
+    render(<About />);
+  });
+
+  // Test 2
+  
+
+})
+
