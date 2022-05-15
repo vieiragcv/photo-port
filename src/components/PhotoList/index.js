57d486a4b31b2117020e456f5a785370363e9dd2
vieiragcv <95590262+vieiragcv@users.noreply.github.com>
2022-05-14 23:17:51 -0400

    pending tests and modal open/close

--- src/components/PhotoList/index.js
+++ src/components/PhotoList/index.js
@@ -1,7 +1,10 @@
 import React, { useState } from 'react';
+import Modal from '../Modal';
 
 
 const PhotoList = ({ category }) => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [currentPhoto, setCurrentPhoto] = useState();
 
   const [photos] = useState([
     {
@@ -103,8 +106,16 @@ const PhotoList = ({ category }) => {
 
   const currentPhotos = photos.filter((photo) => photo.category === category);
   
+  const toggleModal = (image, i) => {
+    setCurrentPhoto({ ...image, index: i });
+    setIsModalOpen(!isModalOpen);
+  };
+
   return (
     <div>
+      {isModalOpen && (
+        <Modal currentPhoto={currentPhoto} onClose={toggleModal}  />
+      )}
       <div className="flex-row">
         {currentPhotos.map((image, i) => (  
           <img
