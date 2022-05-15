2be1eda8a5ecf5f15a07cd704c049caabaf65082
vieiragcv <95590262+vieiragcv@users.noreply.github.com>
2022-05-14 20:31:23 -0400

    finalized gallery rendering and tests

--- src/components/About/__tests__/index.test.js
+++ src/components/About/__tests__/index.test.js
@@ -1,22 +1,20 @@
 import React from 'react';
 import { render, cleanup } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
-
 import About from '..';
 
 afterEach(cleanup);
 
-describe('About component', () => {
-
-  // Test 1
+describe('About component renders', () => {
   it('renders', () => {
     render(<About />);
   });
-
-  // Test 2
-  it('matches snapshot DOM node structures', () => {
+  
+  it('matches snapshot DOM node structure', () => {
     const { asFragment } = render(<About />);
+    
     expect(asFragment()).toMatchSnapshot();
-  })
+  });
 })
+
 
--- src/components/Gallery/__tests__/index.tests.js
+++ src/components/Gallery/__tests__/index.tests.js
@@ -1,18 +1,24 @@
-import React from 'react';
-import { render, cleanup } from '@testing-library/react';
-import '@testing-library/jest-dom/extend-expect';
+import React from 'react'
+import { render, cleanup } from '@testing-library/react'
+import '@testing-library/jest-dom/extend-expect'
+import Gallery from '..'
+const portrait = { name: "portraits", description: "Portraits of people in my life" };
 
-import Gallery from '..';
-
-afterEach(cleanup);
+afterEach(cleanup)
 
 describe('Gallery component', () => {
 
-  //TEST 1
   it('renders', () => {
-    render(<Gallery />);
+    render(<Gallery currentCategory={portrait} />);
   });
 
-  //TEST 2
-  
+  it('matches snapshot', () => {
+    const { asFragment } = render(<Gallery currentCategory={portrait} />)
+    expect(asFragment()).toMatchSnapshot()
+  })
 });
+
+it('renders', () => {
+  const { getByTestId } = render(<Gallery currentCategory={portrait} />)
+  expect(getByTestId('h1tag')).toHaveTextContent('Portraits')
+})
